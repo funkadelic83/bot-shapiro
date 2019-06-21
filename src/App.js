2e0e7d0201f3635a55dc660530b7e30830771b54
unknown <mikedamanskis@gmail.com>
2019-06-21 12:44:50 -0700

    inital logic

--- src/App.js
+++ src/App.js
@@ -2,6 +2,11 @@ import React from 'react';
 import './App.css';
 import ChatBot from 'react-simple-chatbot';
 
+var name;
+// let insults = 0;
+// let validArg = 0;
+// let unknown = 0;
+
 function App() {
   return (
     <div>
@@ -17,41 +22,45 @@ function App() {
             user: true,
             validator: (value) => {
               if (value.length > 15) {
-                  return 'Too long!';
+                return 'You expect me to pronounce that?';
               } if (value.includes(" ")) {
                 return 'One word names only please, liberal.';
               }
               return true;
-          },
+            },
             trigger: (value) => {
-              if (value.steps.firstName.value.toLowerCase() === 'mike') {
-                return '4'
+              name = value.steps.firstName.value;
+              if (
+                value.steps.firstName.value.toLowerCase() === 'ben' ||
+                value.steps.firstName.value.toLowerCase() === 'benjamin'
+              ) {
+                return 'nameBen'
               } else if (
-                value.steps.firstName.value.toLowerCase() === 'chad' || 
-                value.steps.firstName.value.toLowerCase() === 'josh' || 
-                value.steps.firstName.value.toLowerCase() === 'john' || 
-                value.steps.firstName.value.toLowerCase() === 'michael' || 
-                value.steps.firstName.value.toLowerCase() === 'christopher' || 
-                value.steps.firstName.value.toLowerCase() === 'chris' || 
-                value.steps.firstName.value.toLowerCase() === 'joshua' || 
-                value.steps.firstName.value.toLowerCase() === 'andy' || 
-                value.steps.firstName.value.toLowerCase() === 'daniel' || 
-                value.steps.firstName.value.toLowerCase() === 'dan' || 
-                value.steps.firstName.value.toLowerCase() === 'james' || 
-                value.steps.firstName.value.toLowerCase() === 'jim' || 
-                value.steps.firstName.value.toLowerCase() === 'nick' || 
-                value.steps.firstName.value.toLowerCase() === 'joseph' || 
-                value.steps.firstName.value.toLowerCase() === 'joe' || 
-                value.steps.firstName.value.toLowerCase() === 'nicholas' || 
-                value.steps.firstName.value.toLowerCase() === 'nick' || 
-                value.steps.firstName.value.toLowerCase() === 'ryan' || 
-                value.steps.firstName.value.toLowerCase() === 'jacob' || 
+                value.steps.firstName.value.toLowerCase() === 'chad' ||
+                value.steps.firstName.value.toLowerCase() === 'josh' ||
+                value.steps.firstName.value.toLowerCase() === 'john' ||
+                value.steps.firstName.value.toLowerCase() === 'michael' ||
+                value.steps.firstName.value.toLowerCase() === 'christopher' ||
+                value.steps.firstName.value.toLowerCase() === 'chris' ||
+                value.steps.firstName.value.toLowerCase() === 'joshua' ||
+                value.steps.firstName.value.toLowerCase() === 'andy' ||
+                value.steps.firstName.value.toLowerCase() === 'daniel' ||
+                value.steps.firstName.value.toLowerCase() === 'dan' ||
+                value.steps.firstName.value.toLowerCase() === 'james' ||
+                value.steps.firstName.value.toLowerCase() === 'jim' ||
+                value.steps.firstName.value.toLowerCase() === 'nick' ||
+                value.steps.firstName.value.toLowerCase() === 'joseph' ||
+                value.steps.firstName.value.toLowerCase() === 'joe' ||
+                value.steps.firstName.value.toLowerCase() === 'nicholas' ||
+                value.steps.firstName.value.toLowerCase() === 'nick' ||
+                value.steps.firstName.value.toLowerCase() === 'ryan' ||
+                value.steps.firstName.value.toLowerCase() === 'jacob' ||
                 value.steps.firstName.value.toLowerCase() === 'brandon' ||
-                value.steps.firstName.value.toLowerCase() === 'andy' || 
-                value.steps.firstName.value.toLowerCase() === 'justin' || 
-                value.steps.firstName.value.toLowerCase() === 'robert' || 
-                value.steps.firstName.value.toLowerCase() === 'rob' || 
-                value.steps.firstName.value.toLowerCase() === 'bob' ||  
+                value.steps.firstName.value.toLowerCase() === 'andy' ||
+                value.steps.firstName.value.toLowerCase() === 'justin' ||
+                value.steps.firstName.value.toLowerCase() === 'robert' ||
+                value.steps.firstName.value.toLowerCase() === 'rob' ||
+                value.steps.firstName.value.toLowerCase() === 'bob' ||
                 value.steps.firstName.value.toLowerCase() === 'william' ||
                 value.steps.firstName.value.toLowerCase() === 'will' ||
                 value.steps.firstName.value.toLowerCase() === 'anthony' ||
@@ -67,35 +76,34 @@ function App() {
                 value.steps.firstName.value.toLowerCase() === 'noah' ||
                 value.steps.firstName.value.toLowerCase() === 'liam' ||
                 value.steps.firstName.value.toLowerCase() === 'zach'
-                )
-                {
+              ) {
                 return 'maleName';
               } else if (
-                value.steps.firstName.value.toLowerCase() === 'jessica' || 
-                value.steps.firstName.value.toLowerCase() === 'jess' || 
-                value.steps.firstName.value.toLowerCase() === 'ashley' || 
-                value.steps.firstName.value.toLowerCase() === 'amanda' || 
-                value.steps.firstName.value.toLowerCase() === 'sarah' || 
-                value.steps.firstName.value.toLowerCase() === 'sara' || 
-                value.steps.firstName.value.toLowerCase() === 'jennifer' || 
-                value.steps.firstName.value.toLowerCase() === 'emily' || 
-                value.steps.firstName.value.toLowerCase() === 'samantha' || 
-                value.steps.firstName.value.toLowerCase() === 'elizabeth' || 
-                value.steps.firstName.value.toLowerCase() === 'liz' || 
-                value.steps.firstName.value.toLowerCase() === 'stephanie' || 
-                value.steps.firstName.value.toLowerCase() === 'steph' || 
-                value.steps.firstName.value.toLowerCase() === 'lauren' || 
-                value.steps.firstName.value.toLowerCase() === 'nichole' || 
-                value.steps.firstName.value.toLowerCase() === 'brittany' || 
-                value.steps.firstName.value.toLowerCase() === 'megan' || 
-                value.steps.firstName.value.toLowerCase() === 'meghan' || 
-                value.steps.firstName.value.toLowerCase() === 'rachel' || 
+                value.steps.firstName.value.toLowerCase() === 'jessica' ||
+                value.steps.firstName.value.toLowerCase() === 'jess' ||
+                value.steps.firstName.value.toLowerCase() === 'ashley' ||
+                value.steps.firstName.value.toLowerCase() === 'amanda' ||
+                value.steps.firstName.value.toLowerCase() === 'sarah' ||
+                value.steps.firstName.value.toLowerCase() === 'sara' ||
+                value.steps.firstName.value.toLowerCase() === 'jennifer' ||
+                value.steps.firstName.value.toLowerCase() === 'emily' ||
+                value.steps.firstName.value.toLowerCase() === 'samantha' ||
+                value.steps.firstName.value.toLowerCase() === 'elizabeth' ||
+                value.steps.firstName.value.toLowerCase() === 'liz' ||
+                value.steps.firstName.value.toLowerCase() === 'stephanie' ||
+                value.steps.firstName.value.toLowerCase() === 'steph' ||
+                value.steps.firstName.value.toLowerCase() === 'lauren' ||
+                value.steps.firstName.value.toLowerCase() === 'nichole' ||
+                value.steps.firstName.value.toLowerCase() === 'brittany' ||
+                value.steps.firstName.value.toLowerCase() === 'megan' ||
+                value.steps.firstName.value.toLowerCase() === 'meghan' ||
+                value.steps.firstName.value.toLowerCase() === 'rachel' ||
                 value.steps.firstName.value.toLowerCase() === 'hannah' ||
-                value.steps.firstName.value.toLowerCase() === 'hanna' || 
-                value.steps.firstName.value.toLowerCase() === 'kayla' || 
-                value.steps.firstName.value.toLowerCase() === 'melissa' || 
-                value.steps.firstName.value.toLowerCase() === 'amber' || 
-                value.steps.firstName.value.toLowerCase() === 'danielle' ||  
+                value.steps.firstName.value.toLowerCase() === 'hanna' ||
+                value.steps.firstName.value.toLowerCase() === 'kayla' ||
+                value.steps.firstName.value.toLowerCase() === 'melissa' ||
+                value.steps.firstName.value.toLowerCase() === 'amber' ||
+                value.steps.firstName.value.toLowerCase() === 'danielle' ||
                 value.steps.firstName.value.toLowerCase() === 'taylor' ||
                 value.steps.firstName.value.toLowerCase() === 'eleanor' ||
                 value.steps.firstName.value.toLowerCase() === 'susan' ||
@@ -112,7 +120,7 @@ function App() {
                 value.steps.firstName.value.toLowerCase() === 'erica' ||
                 value.steps.firstName.value.toLowerCase() === 'heather' ||
                 value.steps.firstName.value.toLowerCase() === 'stacy'
-                ) {
+              ) {
                 return 'femaleName';
               } else {
                 return 'unknownName'
@@ -121,26 +129,55 @@ function App() {
           },
           {
 
-            id: '4',
-            message: `Hello Mike`,
-            end: true
+            id: 'nameBen',
+            message: `That is a truly great name. But MY wife is a doctor. I will DESTROY you with FACTS and LOGIC. What topic would you like to debate?`,
+            trigger: 'debateTopic'
           },
           {
 
             id: 'unknownName',
             message: `What kind of name is {previousValue}? Liberals have more names than they have genders! What is your preferred pronoun?`,
-            end: true
+            trigger: 'unknownGender'
+          },
+          {
+            id: 'unknownGender',
+            options: [
+              { value: 'male', label: 'Male', trigger: 'maleName' },
+              { value: 'female', label: 'Female', trigger: 'femaleName' },
+              { value: 'nonBinary', label: 'Other', trigger: 'nonBinary'}
+            ]
           },
           {
             id: 'maleName',
-            message: `You must be a dude.`,
-            end: true
+            message: `Greetings ${name}. I am popular an no one's ever heard of you. My wife is a doctor. I will destroy you with FACTS and LOGIC. What topic would you like to debate?`,
+            trigger: 'debateTopic'
           },
-{
-  id: 'femaleName',
-  message: `You sound like a feminist.`,
-  end: true,
-}
+           {
+            id: 'debateTopic',
+            options: [
+              { value: 'gender', label: 'Gender', trigger: 'genderDebate' }
+            ]
+           },
+           {
+             id: 'genderDebate',
+             message: 'You must be a boy to be a boy. It is in the name, boy.',
+             trigger: 'genderResponse1'
+           },
+           {
+             id: 'genderResponse1',
+             message: 'lol',
+            end: true
+           },
+          {
+            id: 'femaleName',
+            message: `There was no War on Women until now. I will DESTROY you with FACTS and LOGIC. Which topic would you like to debate?`,
+            trigger: 'debateTopic'
+          },
+          {
+            id: 'nonBinary',
+            message: `If you whip out your female penis next to my small daughter in a ladie's room, I don't care what you think you are. I will DESTROY you with FACTS and LOGIC. What topic would you like to debate?`,
+            end: true
+          }
 
         ]}
       />
